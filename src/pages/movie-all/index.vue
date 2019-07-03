3144166aed93c36ee792c57549834be5d56e3942
136shine <1262359762@qq.com>
2019-07-03 10:18:11 +0800

    modify movie-all

--- src/pages/movie-all/index.vue
+++ src/pages/movie-all/index.vue
@@ -1,13 +1,23 @@
 <template>
-    <movie-item :movieList="movies"></movie-item>
+    <!-- <movie-item :movieList="movies"></movie-item> -->
+    <div class="movieContent" v-if="isShow">
+      <div v-for="item in movies" :key="item.id" class='moiveItem' @click="bindGoDetail(item.id)">
+        <img :src="item.images.small" />
+        <div class="movieDesc">
+          <span class="title">{{item.title}}</span>
+          <span class="grade">{{item.rating.average}}</span>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import movieItem from '@/components/movie-item/index.vue'
+// import movieItem from '@/components/movies-item/index.vue'
 import { URI } from '@/utils/movie-api.js'
 export default {
     data() {
         return {
+            isShow: false,
             page: 1,
             count: 20,
             categery: '',
@@ -15,15 +25,20 @@ export default {
         }
     },
     onShow() {
-        this.categery = this.$root.$mp.query.categery
+        this.categery = this.$root.$mp.query.categery || 'in_theaters'
         this.getMoviesByCategery(this.categery, this.page, this.count)
     },
     methods: {
-        async getMoviesByCategery(categery){
+        async getMoviesByCategery(categery, page, count){
             const params = { start: (page - 1) * count, count: count, city: '北京' }
             await this.$fetch(URI, categery, params).then( res=> {
-                console.log('getMoviesByCategery---res=>', res)
-                this.movies = res.data
+                this.movies = [...this.movies, ...res.data.subjects]
+                this.isShow = true
+            })
+        },
+        bindGoDetail(id) {
+            wx.navigateTo({
+                url: "../movie-detail/main?movieId=" + id
             })
         }
     },
@@ -31,9 +46,57 @@ export default {
         this.page++
         this.getMoviesByCategery(this.categery, this.page, this.count)
     },
-    components: {
-        movieItem
-    }
+    // components: {
+    //     movieItem
+    // }
 }
 </script>
+
+<style scoped>
+.movieContent {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  display: flex;
+  display: -webkit-flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: start;
+  /* justify-content: space-around; */
+}
+
+.moiveItem {
+  display: inline-block;
+  width: 200rpx;
+  height: 380rpx;
+  margin: 18rpx 24rpx;
+}
+.moiveItem image {
+  display: inline-block;
+  width: 100%;
+  height: 300rpx;
+}
+.moiveItem .movieDesc {
+  display: block;
+  width: 100%;
+  overflow: hidden;
+  /* background-color: #faf9f9; */
+  margin: 0px;
+  padding: 0px;
+}
+.movieDesc .title {
+  display: block;
+   width:100%;
+  font-size: 24rpx;
+  line-height: 24rpx;
+  overflow: hidden;
+  span-overflow: ellipsis;
+  white-space: nowrap;
+}
+.movieDesc .grade {
+  font-size: 26rpx;
+  color: #f89606;
+}
+</style>
+
 
